05d47280c1c1d5a0ccba9aeb0a7c7bc403464acb
qq11123334 <qq11123334@csie.io>
2023-07-29 16:49:49 +0800

    change myAlgo2 Fidelity

--- src/Algorithm/MyAlgo2/MyAlgo2.cpp
+++ src/Algorithm/MyAlgo2/MyAlgo2.cpp
@@ -254,7 +254,7 @@ void MyAlgo2::run() {
 
     res["succ_request_cnt"] /= max_xim_sum;
     // res["fidelity_gain"] /= max_xim_sum;
-    res["fidelity_gain"] = res["succ_request_cnt"]ax_xim_sum;
+    res["fidelity_gain"] = res["succ_request_cnt"];
     res["utilization"] = (usage / ((double)memory_total_LP * (double)graph.get_time_limit())) / max_xim_sum;
 
     cerr << "[" << algorithm_name << "] end" << endl;
